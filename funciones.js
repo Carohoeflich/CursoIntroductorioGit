52ca2c75d4228a8dd5a49e345d21f96f9fe79960
Carolina Hoeflich <carohoeflich@hotmail.com>
2021-09-24 22:04:34 -0500

    Cree una modificacion para un alert que dice hola y lo mande a llamar

--- funciones.js
+++ funciones.js
@@ -1,3 +1,4 @@
+// if and else if
 let x = 10;
 let y = 10;
 let z = 15;
@@ -8,3 +9,12 @@ if (x != z){
     } else {
     console.log("X and Z are the same");
 }
+
+
+//cree una funcion pero no la llame
+function mostrarMensaje(){
+    alert("Hola");
+}
+
+//llame la funcion 
+mostrarMensaje();
