ec017e93f05cb7926e777f2459720407e0486740
Carolina Hoeflich <carohoeflich@hotmail.com>
2021-09-24 23:58:46 -0500

    WHY ISNT A SIMPLE FUNCION WORKING

---
+++ funcionanonima.js
@@ -0,0 +1,22 @@
+
+//funcion normal
+/*function square(number){
+    return number * number
+};*/
+
+//funcion anonima en una variable
+/*var hola = function(){
+    console.log("Hola");
+    };
+
+    hola();*/
+
+
+//otra funcion que no es anonima, pero se le hace un llamado    
+
+function saludo(nombre){
+    return console.log("Hola: " + saludo);
+}
+
+nombre = "Carolina";
+saludo(nombre);
