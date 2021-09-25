a17eb1ac63a7facbedd1abccff0e91ce04dc41a8
Carolina Hoeflich <carohoeflich@hotmail.com>
2021-09-24 21:57:22 -0500

    Variables y funciones en javascript para aprender ty

---
+++ funciones.js
@@ -0,0 +1,10 @@
+let x = 10;
+let y = 10;
+let z = 15;
+if (x != z){
+    console.log("X and Z are not the same"); }
+    else if (y<=z){
+        console.log("Y is less than Z");
+    } else {
+    console.log("X and Z are the same");
+}
