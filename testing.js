281081e07ecf45785877f11cc1b4a2ae63d31129
Carolina Hoeflich <carohoeflich@hotmail.com>
2021-09-24 21:27:59 -0500

    modified de testing.js with types of variables

--- testing.js
+++ testing.js
@@ -1,1 +1,7 @@
-alert("Hola");
+let variable = 42; // variable es number
+let variable = "Text"; // string
+let variable = true; // boolean
+let variable = null; // null
+let variable = 0; // still a number, not null.
+let variable // an undefined variable
+let variable = Symbol("Hola"); // this is new¿ called a Symbol
